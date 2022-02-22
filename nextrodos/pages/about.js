2c940a63d4ae318b08bea1edeb92879a2dc968f9
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-22 17:09:36 +0300

    img to center

--- nextrodos/pages/about.js
+++ nextrodos/pages/about.js
@@ -19,13 +19,15 @@ class About extends PureComponent {
       <DefaultLayout>
         <div style={styles.wrapper}>
           <h1 style={{ textAlign: 'center'}}>This page about me.</h1>
+          <div style={{ textAlign: 'center'}}>
           <Image
          
          src="/me.jpg"
          alt="Picture of the author"
-         width={200}
-         height={200}
+         width={250}
+         height={250}
       />
+          </div>
           <h1 style={{ textAlign: 'center'}}>My name is Hamit, and this is my first experience with NextJS, for example.</h1>
           <p>I am a novice node js developer. In 2022, I want to get a job. At the moment I have several working projects, including this one, that I would like to demonstrate in order to impress you.
 At the moment I use such well-known technologies as:</p>
