cb7344308abaddded96a336a4ead605b2aa32929
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-22 17:02:48 +0300

    fix about

--- nextrodos/pages/about.js
+++ nextrodos/pages/about.js
@@ -7,8 +7,10 @@ const styles = {
     backgroundColor: '#fff',
     margin: '40px 20%',
     padding: 40,
-  },
+  }
 };
+
+
 
 // eslint-disable-next-line react/prefer-stateless-function
 class About extends PureComponent {
@@ -16,15 +18,15 @@ class About extends PureComponent {
     return (
       <DefaultLayout>
         <div style={styles.wrapper}>
-          <h1>This page about me.</h1>
+          <h1 style={{ textAlign: 'center'}}>This page about me.</h1>
           <Image
-         layout='responsive'
+         
          src="/me.jpg"
          alt="Picture of the author"
-         width={300}
-         height={300}
+         width={200}
+         height={200}
       />
-          <h1>My name is Hamit, and this is my first experience with NextJS, for example.</h1>
+          <h1 style={{ textAlign: 'center'}}>My name is Hamit, and this is my first experience with NextJS, for example.</h1>
           <p>I am a novice node js developer. In 2022, I want to get a job. At the moment I have several working projects, including this one, that I would like to demonstrate in order to impress you.
 At the moment I use such well-known technologies as:</p>
         <ul>
