d0b86cddec30925624ee282eb8fcd6985f33778d
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-27 16:42:33 +0300

    login component updated

--- authmongo/pages/login.js
+++ authmongo/pages/login.js
@@ -1,22 +1,52 @@
-import { useState } from "react"
+import React, { useState} from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios"
 import cookie from 'js-cookie'
 import { useSession, signIn, signOut, getSession } from "next-auth/react"
 import { useRouter } from "next/router"
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-const Login = () => {
+const theme = createTheme();
+
+function Login() {
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const { data: session } = useSession()
     const router = useRouter()
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-        }
-    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+
         const { data } = await axios.post(
             `/api/login`, 
             { email, password }, 
@@ -27,23 +57,80 @@ const Login = () => {
         router.push('/')
     }
 
-    if(session) {
-        return <>
-          Signed in as {session.user.email} <br/>
-          <button>Sign out</button>
-         
-        </>
-      }
-      return <>
-        Not signed in <br/>
-        <button onClick={() => signIn()}>Sign in</button>
-        <form onSubmit={handleSubmit}>
-                <h1>Login</h1>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Login</button>
-            </form>
-      </>
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <button onClick={() => signIn()}>Sign in</button>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export async function getServerSideProps(context) {
