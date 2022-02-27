57ea330286499c8e498456c22e4996c734a65ad3
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-02-27 11:28:28 +0300

    add material ui to project

---
+++ authmongo/components/Layout.js
@@ -0,0 +1,14 @@
+import Header from "./header"
+import { Container } from '@mui/material'
+
+const Layout = ({children}) => {
+    return (
+        <>
+        <Container>
+            <Header/>
+           {children}
+           </Container>
+        </>
+    )
+}
+export default Layout
