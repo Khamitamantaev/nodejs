3e13bd6aef44648e05c76c9d6a24b94c4e9fc5a7
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-27 12:34:55 +0300

    map react error fix

--- linkedin/components/Form/UserTrees.js
+++ linkedin/components/Form/UserTrees.js
@@ -11,12 +11,13 @@ const UserTrees = () => {
     },[currentTree])
 
   return (
-    <div class="w-3/3 bg-white rounded-lg shadow">
-    <ul >
-    <ul className="divide-y-2 divide-gray-100">{[{"_id":"1","name":"Khamit"},{"_id":"2","name":"Aza"},{"_id":"3","name":"AZAMAT"}].map(tree => (
+    <div className="w-3/3 bg-white rounded-lg shadow">
+      {user_trees.contents && user_trees.contents.length? (
+      <ul className="divide-y-2 divide-gray-100">{user_trees.contents.map(tree => (
         <li className='p-3' onClick={() => setCurrentTree(tree)} key={tree._id}>{tree.name}</li>
     ))}</ul>
-    </ul>
+    ):""}
+    
 </div>
    
   )
