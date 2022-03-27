2165ad531288e83a1b8402aed5659d4e959e9503
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-27 10:53:11 +0300

    small fix UserTree

--- linkedin/components/Form/UserTrees.js
+++ linkedin/components/Form/UserTrees.js
@@ -4,17 +4,21 @@ import { useRecoilState } from 'recoil';
 import { selectedTreeState } from '../../atoms/treeAtom';
 
 const UserTrees = () => {
-
     const [currentTree, setCurrentTree] = useRecoilState(selectedTreeState);
-
+    
     useEffect(() => {
         console.log(currentTree)
     },[currentTree])
 
   return (
-    <div>{[{"_id":"1","name":"Khamit"},{"_id":"2","name":"Aza"},{"_id":"3","name":"AZAMAT"}].map(tree => (
-        <Button onClick={() => setCurrentTree(tree)} key={tree._id}>{tree.name}</Button>
-    ))}</div>
+    <div class="w-3/3 bg-white rounded-lg shadow">
+    <ul >
+    <ul className="divide-y-2 divide-gray-100">{[{"_id":"1","name":"Khamit"},{"_id":"2","name":"Aza"},{"_id":"3","name":"AZAMAT"}].map(tree => (
+        <li className='p-3' onClick={() => setCurrentTree(tree)} key={tree._id}>{tree.name}</li>
+    ))}</ul>
+    </ul>
+</div>
+   
   )
 }
 
