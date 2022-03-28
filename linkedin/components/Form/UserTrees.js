42ffdf9d8a13b9e7dc2c2502177064550282d409
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-28 17:30:51 +0300

    fetch trees working normally to list

--- linkedin/components/Form/UserTrees.js
+++ linkedin/components/Form/UserTrees.js
@@ -1,15 +1,29 @@
 import { Button } from '@mui/material';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRecoilState, useRecoilValueLoadable } from 'recoil';
-import { selectedTreeState, userTreeList } from '../../atoms/treeAtom';
+import { handleTreeState, selectedTreeState, userTreeList } from '../../atoms/treeAtom';
 const UserTrees = () => {
     const [currentTree, setCurrentTree] = useRecoilState(selectedTreeState)
-    const user_trees = useRecoilValueLoadable(userTreeList)
+    // const user_trees = useRecoilValueLoadable(userTreeList)
+    const [trees, setTrees] = useState([])
+    const [handleTree, setHandleTree] = useRecoilState(handleTreeState);
+
+    useEffect(() => {
+      const fetchTrees = async () => {
+        const response = await fetch("/api/tree", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        const responseData = await response.json();
+        setTrees(responseData.trees)
+      };
+      fetchTrees();
+    },[handleTree])
 
   return (
     <div className="w-3/3 bg-white rounded-lg shadow">
-      {user_trees.contents && user_trees.contents.length? (
-      <ul className="divide-y-2 divide-gray-100">{user_trees.contents.map(tree => (
+      {trees && trees.length? (
+      <ul className="divide-y-2 divide-gray-100">{trees.map(tree => (
         <li className='p-3' key={tree._id}><Button onClick={() => setCurrentTree(tree._id)} >{tree.name}</Button></li>
     ))}</ul>
     ):""}
