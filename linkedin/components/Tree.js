81c4a1c01100f0aa99f2157a06f59cf5acf4693c
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-03-31 09:52:48 +0300

    change border

--- linkedin/components/Tree.js
+++ linkedin/components/Tree.js
@@ -57,7 +57,7 @@ export default function OrgChartTree({ data }) {
       {/* `foreignObject` requires width & height to be explicitly set. */}
       <foreignObject {...foreignObjectProps} >
         {nodeDatum._id ?
-          <div className='bg-green-500 hover:bg-sky-400 static'>
+          <div className='bg-green-500 hover:bg-sky-400 static rounded-[18px] border-double border-4 border-indigo-600'>
             <button style={{ width: "100%" }} onClick={() => handleClick(nodeDatum)}>Добавить элемент</button>
             <button disabled={!nodeDatum.parentID} style={{ width: "100%" }} onClick={() => handleDeleteClick(nodeDatum)}>Удалить элемент</button>
             <h3 onClick={() => handleTestClick(nodeDatum)} style={{ textAlign: "center", font: "bold italic large serif", color: "#191970", fontSize: '20px' }}>{nodeDatum.name}</h3>
